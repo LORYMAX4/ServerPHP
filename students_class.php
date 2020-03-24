168f5393f9686f961f2ac9f1596cbb48949bac66
LORYMAX <lorymax4@gmail.com>
2020-03-24 23:40:43 +0100

    Aggiunta classe students_class e reso il codice più leggibile

---
+++ students_class.php
@@ -0,0 +1,137 @@
+<?php
+    $_dbHostname = "localhost:3306";
+    $_dbName = "fi_itis_meucci";
+    $_dbUsername = "root";
+    $_dbPassword = "root";
+    $_con = new PDO("mysql:host=$_dbHostname;dbname=$_dbName", $_dbUsername, $_dbPassword);
+    $_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $_con->exec('SET NAMES utf8');
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+    switch($requestMethod)
+    {
+        case 'GET':
+            $pathArray = explode('/',$_SERVER['REQUEST_URI']);
+            if(isset($pathArray[2]))
+            {
+                //con parametro id
+                $id = $pathArray[2];
+                $sql = "select * from student_class where id=:id";
+                $stmt = $_con->prepare($sql);
+				$params = 
+				[
+                    'id'=>$id
+                ];
+                $stmt->execute($params);
+                $data = $stmt->fetch(\PDO::FETCH_ASSOC);
+            }
+            else
+            {
+                //senza il parametro id torna tutta la tabella
+                $sql = "select * from student_class";
+                $stmt = $_con->prepare($sql);
+                $stmt->execute();
+                $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            }
+            $js_encode = json_encode(array($data),true);
+            header('Content-Type: application/json');
+            echo $js_encode;
+        break;
+		case 'POST':
+			$json = file_get_contents('php://input');
+			$data = json_decode($json,true);
+			$sql = "insert into student_class values(default, :idStudent, :idClass);";
+			$stmt = $_con->prepare($sql);
+			$params = 
+			[
+				'idStudent'=>$data["idStudent"],
+				'idClass'=>$data["idClass"]
+			];
+			$stmt->execute($params);
+			$sql = "select * from student_class where idStudent=:idStudent and idClass=:idClass";
+			$stmt = $_con->prepare($sql);
+			$params = 
+			[
+				'idStudent'=>$data["idStudent"],
+				'idClass'=>$data["idClass"]
+			];
+			$stmt->execute($params);
+			$data = $stmt->fetch(\PDO::FETCH_ASSOC);
+			$js_encode = json_encode(array($data),true);
+            header('Content-Type: application/json');
+            echo $js_encode;	
+		break;
+		case 'DELETE':
+			$pathArray = explode('/',$_SERVER['REQUEST_URI']);
+			$id=$pathArray[2];
+			$sql = 'delete from student_class where id=:id';
+			$stmt = $_con->prepare($sql);
+			$params = 
+			[
+                'id'=>$id
+            ];
+			$stmt->execute($params);
+			echo 'Cancellazione effettuata.';
+		break;
+		case 'PUT':
+			$pathArray = explode('/',$_SERVER['REQUEST_URI']);
+			$id=$pathArray[2];
+			$json = file_get_contents('php://input');
+			$data = json_decode($json,true);
+			$sql = "update student_class set idStudent=:idStudent, idClass=:idClass where id=:id";
+			$stmt = $_con->prepare($sql);
+			if(!isset($data['idStudent']) || !isset($data['idClass']))
+			{
+				echo 'I campi non possono essere vuoti.';
+				break;
+			}
+			$params = 
+			[
+				'idStudent'=>$data['idStudent'],
+				'idClass'=>$data['idClass'],
+				'id'=>$id
+            ];
+			$stmt->execute($params);
+			$sql = 'select * from student_class where id=:id';
+			$stmt = $_con->prepare($sql);
+			$params =
+			[
+				'id'=>$id
+			];
+			$stmt->execute($params);
+			$data = $stmt->fetch(\PDO::FETCH_ASSOC);
+			$js_encode = json_encode(array($data),true);
+            header('Content-Type: application/json');
+            echo $js_encode;
+		break;
+		case 'PATCH':
+			$pathArray = explode('/',$_SERVER['REQUEST_URI']);
+			$id=$pathArray[2];
+			$json = file_get_contents('php://input');
+			$data = json_decode($json,true);
+			$sql = 'update student_class set ';
+			if($data['idStudent']!="")
+			{
+				$sql = $sql . 'idStudent="' . $data['idStudent'] . '",';
+			}
+			if($data['idClass']!="")
+			{
+				$sql = $sql . 'idClass="' . $data['idClass'] . '",';
+			}
+			$sql = substr($sql, 0, strlen($sql)-1);
+			$sql = $sql . ' where id=' . $id;
+			$stmt = $_con->prepare($sql);
+			$stmt->execute();
+			$sql = 'select * from student_class where id=:id';
+			$stmt = $_con->prepare($sql);
+			$params = 
+			[
+				'id'=>$id
+			];
+			$stmt->execute($params);
+			$data = $stmt->fetch(\PDO::FETCH_ASSOC);
+			$js_encode = json_encode(array($data),true);
+            header('Content-Type: application/json');
+            echo $js_encode;
+		break;
+    }
+?> 
