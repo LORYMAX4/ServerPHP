2ae1c9becea2a326c86f05776c7d17aa77abd875
LORYMAX <lorymax4@gmail.com>
2020-03-30 23:41:51 +0200

    Implementata parte grafica

---
+++ class/DBConnection.php
@@ -0,0 +1,27 @@
+<?php
+class DBConnection
+{
+    private $_dbHostname = "localhost:3306";
+    private $_dbName = "fi_itis_meucci";
+    private $_dbUsername = "root";
+    private $_dbPassword = "root";
+    private $_con;
+    public function __construct()
+    {
+      try
+      {
+        	$this->_con = new PDO("mysql:host=$this->_dbHostname;dbname=$this->_dbName", $this->_dbUsername, $this->_dbPassword);    
+          $this->_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $this->_con->exec('SET NAMES utf8');
+      } 
+      catch(PDOException $e)
+      {
+			echo "Connection failed: " . $e->getMessage();
+		  }
+    }
+    public function returnConnection()
+    {
+        return $this->_con;
+    }
+}
+?>
---
+++ class/Student.php
@@ -0,0 +1,169 @@
+<?php
+include("DBConnection.php");
+class Student 
+{
+    protected $db;
+    public $_id;
+    public $_name;
+    public $_surname;
+    public $_sidiCode;
+    public $_taxCode;
+	public function __construct()
+	{
+        $this->db = new DBConnection();
+        $this->db = $this->db->returnConnection();
+    }
+	public function insert()
+	{
+		try
+		{
+    		$sql = 'INSERT INTO student (name, surname, sidi_code, tax_code)  VALUES (:name, :surname, :sidi_code, :tax_code)';
+			$data = 
+			[
+			    'name' => $this->_name,
+			    'surname' => $this->_surname,
+			    'sidi_code' => $this->_sidiCode,
+			    'tax_code' => $this->_taxCode
+			];
+	    	$stmt = $this->db->prepare($sql);
+	    	$stmt->execute($data);
+			$status = $stmt->rowCount();
+            return $status;
+		} 
+		catch (Exception $e)
+		{
+    		die("Oh noes! There's an error in the query! ".$e);
+		}
+    }
+	public function list()
+	{
+		try
+		{
+    		$sql = "SELECT * FROM student";
+		    $stmt = $this->db->prepare($sql);
+		    $stmt->execute();
+		    $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+		} 
+		catch (Exception $e)
+		{
+		    die("Oh noes! There's an error in the query! ".$e);
+		}
+    }
+	public function one() 
+	{
+		try 
+		{
+    		$sql = "SELECT * FROM student WHERE id=:id";
+		    $stmt = $this->db->prepare($sql);
+			$data = 
+			[
+		    	'id' => $this->_id
+			];
+		    $stmt->execute($data);
+		    $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+            return $result;
+		} 
+		catch (Exception $e)
+		{
+		    die("Oh noes! There's an error in the query! ".$e);
+		}
+	}
+	public function getLast()
+	{
+		try
+		{
+    		$sql = "SELECT * FROM student ORDER BY id DESC LIMIT 1";
+		    $stmt = $this->db->prepare($sql);
+		    $stmt->execute($data);
+		    $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+            return $result;
+		}
+		catch (Exception $e)
+		{
+		    die("Oh noes! There's an error in the query! ".$e);
+		}
+	}
+	public function delete()
+	{
+		try
+		{
+    		$sql = "DELETE FROM student_class WHERE id_student=:id";
+		    $stmt = $this->db->prepare($sql);
+			$data = 
+			[
+		    	'id' => $this->_id
+			];
+		    $stmt->execute($data);
+			$status = $stmt->rowCount();
+			$sql = "DELETE FROM student WHERE id=:id";
+			$stmt = $this->db->prepare($sql);
+			$data = 
+			[
+		    	'id' => $this->_id
+			];
+		    $stmt->execute($data);
+			$status = $stmt->rowCount();
+            return $status;
+		}
+		catch (Exception $e)
+		{
+			die("Oh noes! There's an error in the query! ".$e);
+		}
+    }
+	public function patch()
+	{
+		try
+		{
+			$data = 
+			[
+				'id' => $this->_id
+			];
+			$sql = 'UPDATE student SET ';
+			foreach($this as $key => $value)
+			{
+				if($value != null && strcmp($key,'db')!=0 && strcmp($key,'id')!=0)
+				{
+					$key = ltrim($key, "_");
+					$sql = $sql . "$key=:$key,";
+
+					$data[$key] = $value;
+				}
+			}
+			$sql = rtrim($sql,",");
+			$sql = $sql . " WHERE id=:id";
+	    	$stmt = $this->db->prepare($sql);
+	    	$stmt->execute($data);
+			$status = $stmt->rowCount();
+			return $status;
+		}
+		catch (Exception $e)
+		{
+    		die("Oh noes! There's an error in the query! ".$e);
+		}
+	}
+	public function put()
+	{
+		try
+		{
+			$sql = 'UPDATE student SET name=:name, surname=:surname, sidi_code=:sidi_code, tax_code=:tax_code WHERE id=:id';
+			$data = 
+			[
+				'id' =>$this->_id,
+			    'name' => $this->_name,
+			    'surname' => $this->_surname,
+			    'sidi_code' => $this->_sidiCode,
+			    'tax_code' => $this->_taxCode
+			];
+	    	$stmt = $this->db->prepare($sql);
+	    	$stmt->execute($data);
+			$status = $stmt->rowCount();
+            return $status;
+		}
+		catch (Exception $e)
+		{
+    		die("Oh noes! There's an error in the query! ".$e);
+		}
+    }
+}
+?>
